0309f81dc885ee70ff24e87625d15fc4ef042e46
davidsheridan3 <davidsheridan061@gmail.com>
2026-04-16 23:01:44 +0100

    Day 18 - Random Walk (introducing random rgb colours)

--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -1,15 +1,21 @@
-from turtle import Turtle, Screen
+import turtle
 import random
 
-move = Turtle()
+move = turtle.Turtle()
+turtle.colormode(255)
 
-colours = ['red', 'blue', 'green', 'yellow', 'cyan', 'magenta']
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b) #tuple
+
 directions = [0, 90, 180, 270]
 move.pensize(15) #adjusting line width
 move.speed("fastest") #adjusting speed of animation
 
 
 for _ in range(200):
-    move.color(random.choice(colours))
+    move.color(random_color()) #introducing completely random, instead of previous list
     move.forward(30)
     move.setheading(random.choice(directions))
