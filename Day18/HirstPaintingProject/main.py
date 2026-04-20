b16c43795998f9d20c01cf394a414a6dc0567291
davidsheridan3 <davidsheridan061@gmail.com>
2026-04-20 20:15:32 +0100

    Day 18 - Creating my own Hirst Painting (project completion)

--- Day18/HirstPaintingProject/main.py
+++ Day18/HirstPaintingProject/main.py
@@ -1,15 +1,28 @@
-# import colorgram
-#
-# rgb_colors = []
-# colors = colorgram.extract('img.jpg', 30)
-# print(colors)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
+import turtle
+import random
 
+turtle.colormode(255)
+tim = turtle.Turtle()
+tim.speed("fastest")
+tim.penup() # removing lines
+tim.hideturtle() # removing arrow
 color_list = [(236, 35, 108), (221, 231, 237), (145, 28, 66), (230, 237, 232), (239, 75, 35), (7, 148, 95), (220, 171, 45), (183, 158, 47), (45, 191, 232), (28, 127, 194), (254, 223, 0), (125, 192, 78), (85, 27, 91), (243, 218, 56), (178, 40, 98), (44, 170, 114), (211, 132, 166), (206, 57, 35), (239, 162, 193), (145, 27, 25), (243, 167, 156), (163, 211, 178), (26, 187, 225), (6, 116, 54), (138, 210, 232), (74, 135, 184), (170, 191, 221), (114, 10, 8)]
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = turtle.Screen()
+screen.exitonclick()
