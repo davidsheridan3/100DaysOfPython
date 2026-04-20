c7fd473c36efcf6a141a1700d56402bd62cbabd8
davidsheridan3 <davidsheridan061@gmail.com>
2026-04-20 19:50:16 +0100

    Day 18 - Creating my own Hirst Painting (color extraction from an existing one online)

--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -8,14 +8,7 @@ def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-    return (r,g,b) #tuple
+    color =  (r,g,b)
+    return color
 
-directions = [0, 90, 180, 270]
-move.pensize(15) #adjusting line width
-move.speed("fastest") #adjusting speed of animation
-
-
-for _ in range(200):
-    move.color(random_color()) #introducing completely random, instead of previous list
-    move.forward(30)
-    move.setheading(random.choice(directions))
+move.speed("fastest")
