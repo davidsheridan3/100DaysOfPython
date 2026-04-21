116aba5dd15d51a48094ca135811d43bb2403c36
davidsheridan3 <davidsheridan061@gmail.com>
2026-04-22 00:40:26 +0100

    Day 19 - Turtle Race Finish

--- Day19/turtle_coordinate_system.py
+++ Day19/turtle_coordinate_system.py
@@ -1,19 +1,36 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(500, 400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "blue", "green", "yellow", "orange", "purple"]
 y_postions = [-70, -40, -10, 20, 50, 80]
-
-
-
+all_turtles = []
 
 for each_color in colors:
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(each_color)
-    tim.goto(-230, y_postions[colors.index(each_color)])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(each_color)
+    new_turtle.goto(-230, y_postions[colors.index(each_color)])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print("You win!")
+            else:
+                print("You lose!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 
 
