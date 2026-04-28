fdca6ba420882fcb1286c206b26ca91a7a62a9d7
davidsheridan3 <davidsheridan061@gmail.com>
2026-04-28 21:23:04 +0100

    Day 20 - Task progress

--- Day20/snake_game/main.py
+++ Day20/snake_game/main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(600, 600) #defining screen dimensions
 screen.bgcolor("black") # black background
 screen.title("Snake Game")
+screen.tracer(0)
 
 # segment_1 = Turtle(shape="square")
 # segment_1.color("white") # adapting colour of square to background
@@ -20,11 +22,23 @@ screen.title("Snake Game")
 
 starting_positions = [(0, 0),(20, 0),(-20, 0)]
 
+segments = []
+
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
+
+
+game_is_on = True
+while game_is_on:
+    screen.update() # only update screen once all segments have moved forward
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
 
 
 
